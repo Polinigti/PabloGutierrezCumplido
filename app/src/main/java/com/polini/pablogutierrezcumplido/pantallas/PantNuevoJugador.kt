174be54147739c1c348a3682ba59e01ad366742c
Pablo Gutiérrez <polinigti11@gmail.com>
2026-02-11 19:16:27 +0100

    último Commit

--- app/src/main/java/com/polini/pablogutierrezcumplido/pantallas/PantNuevoJugador.kt
+++ app/src/main/java/com/polini/pablogutierrezcumplido/pantallas/PantNuevoJugador.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -21,7 +25,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun PantNuevoJugador(){
+fun PantNuevoJugador(email: String,onAHome: () -> Unit){
 
     Column(
         modifier = Modifier
