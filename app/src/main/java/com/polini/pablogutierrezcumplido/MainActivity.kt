9d92853a6b29848c424e78540333ea66dfa633b7
Pablo Gutiérrez <polinigti11@gmail.com>
2026-02-11 16:38:13 +0100

    Segundo Commit

--- app/src/main/java/com/polini/pablogutierrezcumplido/MainActivity.kt
+++ app/src/main/java/com/polini/pablogutierrezcumplido/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.polini.pablogutierrezcumplido.pantallas.PantLogin
 import com.polini.pablogutierrezcumplido.ui.theme.PabloGutierrezCumplidoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PabloGutierrezCumplidoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PantLogin()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PabloGutierrezCumplidoTheme {
-        Greeting("Android")
     }
 }
